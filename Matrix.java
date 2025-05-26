f74498d6e17c2f12b39b3be768543c60767747e8
[DELL] <[sejalsavran2004@gmail.com]>
2025-05-26 06:29:00 -0700

    done some changes in matrix

--- Matrix.java
+++ Matrix.java
@@ -1,3 +1,40 @@
+
+import java.util.*;
 public class Matrix {
+
+    public static boolean search(int arr[][],int key){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                if(arr[i][j]==key){
+                    System.out.println("Element found.");
+                    return true;
+                }
+
+            }
+        }
+        
+        System.out.println("Element not found");
+        return false;
+    }
+
+    public static void main(String [] args){
+        int arr[][]=new int[3][3];
+        int n=3;
+        int m=3;
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+          for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+
+        }
+        search(arr,5);
+    }
     
 }
