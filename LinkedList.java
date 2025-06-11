0b1654d7de5a3410d0d5841844b6564c30282a3d
[DELL] <[sejalsavran2004@gmail.com]>
2025-06-10 23:29:28 -0700

    Added Frequen Number from a ArrayList

---
+++ LinkedList.java
@@ -0,0 +1,24 @@
+public class LinkedList {
+
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+ public static Node head;
+ public static Node tail;
+
+ public void addFirst(int data)
+
+    public static void main(String [] args){
+        LinkedList L1=new LinkedList();
+        L1.head=new Node(1);
+        L1.head.next=new Node(2);
+
+    }
+    
+}
