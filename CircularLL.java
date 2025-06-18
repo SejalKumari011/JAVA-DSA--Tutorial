60f7e25f6591f54f05e57017abd1c12d128a73b9
[DELL] <[sejalsavran2004@gmail.com]>
2025-06-18 06:36:48 -0700

    Added Circular LinkedList

--- CircularLL.java
+++ CircularLL.java
@@ -14,6 +14,8 @@ public class CircularLL {
     private Node tail=null;
     private int size=0;
 
+    //add at start
+
     public void addAtStart(int data){
         Node newnode=new Node(data);
 
@@ -31,6 +33,74 @@ public class CircularLL {
         size++;
 
     }
+
+    //add at end
+    public void addAtEnd(int data){
+        Node newnode=new Node(data);
+
+        if(head==null){
+            head=tail=newnode;
+            newnode.next=head;
+            tail.next=head;
+            size++;
+            //return tail;
+        }
+
+        newnode.next=tail.next;
+        tail.next=newnode;
+        tail=newnode;
+        //return tail;
+    }
+
+    //insertion between two nodes
+
+    public void addInBetween(int data){
+        Node newnode=new Node(data);  
+        newnode.next=head.next;
+        head.next=newnode;
+        size++;
+    }
+
+
+    //delete first node
+
+    public void deleteFirst(){
+       // Node newnode=new Node(data);
+
+        if(head==null){
+            System.out.println("CLL is already empty.");
+            return;
+        }
+
+        if(head==tail){
+            head=tail=null;
+        }
+
+        head=head.next;
+        tail.next=head;
+    }
+
+    //delete last node
+
+    public void deleteLast(){
+        if(head==null){
+            System.out.println("CLL is empty.");
+            return;
+        }
+
+        if(head==tail){
+            head=tail=null;
+        }
+        
+        Node curr=head;
+
+        while(curr.next!=tail){
+            curr=curr.next;
+        }
+        curr.next=head;
+        tail=curr;
+    }
+
 
 
     public void print(){
@@ -54,8 +124,14 @@ public class CircularLL {
         cll.addAtStart(3);
         cll.addAtStart(2);
         cll.addAtStart(1);
-
+        cll.addAtEnd(4);
+        cll.addInBetween(12);
         cll.print();
+        cll.deleteFirst();
+        cll.print();
+        cll.deleteLast();
+        cll.print();
+      
 
     }
     
