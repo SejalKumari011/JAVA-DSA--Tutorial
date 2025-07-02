3035d8e54e215cec665e3dbb1f82fd8bad754ca4
[DELL] <[sejalsavran2004@gmail.com]>
2025-07-02 07:50:20 -0700

    added queue

---
+++ Queue.java
@@ -0,0 +1,74 @@
+public class Queue {
+
+    static class queue{
+         static int arr[];
+         static int size;
+         static int rear;
+
+         queue(int n){
+            arr=new int[n];
+            size=n;
+            rear=-1;
+
+         }
+
+         public static boolean isEmpty(){
+            return rear==-1;
+         }
+  6
+         //add
+         public static void add(int data){
+
+            if(rear==size-1){
+                System.out.println("Queue is Full");
+                return;
+            }
+
+            rear=rear+1;
+            arr[rear]=data;
+
+         }
+
+         //remove
+
+         public static int remove(){
+
+            if(rear==size-1){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+
+            int front=arr[0];
+            for(int i=0;i<rear;i++){
+                arr[i]=arr[i+1];
+            }
+            rear=rear-1;
+            return front;
+         }
+
+         //peek
+         public static int peek(){
+            if(rear==size-1){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return arr[0];
+
+         }
+
+    }
+
+    public static void main(String [] args){
+        queue q=new queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+
+    }
+    
+}
