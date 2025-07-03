31a6b49fab8561fdb67eed02b66a36d278e7fb44
[DELL] <[sejalsavran2004@gmail.com]>
2025-07-03 08:02:43 -0700

    added problem count number of even and odd

--- Queue.java
+++ Queue.java
@@ -4,27 +4,36 @@ public class Queue {
          static int arr[];
          static int size;
          static int rear;
+         static int front;
 
          queue(int n){
             arr=new int[n];
             size=n;
             rear=-1;
-
+            front=-1;
          }
 
          public static boolean isEmpty(){
-            return rear==-1;
+            return rear==-1 && front==-1;
          }
-  6
+
+         public static boolean isFull(){
+            return (rear+1)%size==front;
+         }
+
          //add
          public static void add(int data){
 
-            if(rear==size-1){
+            if(isFull()){
                 System.out.println("Queue is Full");
                 return;
             }
 
-            rear=rear+1;
+
+            if(front==-1){
+                front=0;
+            }
+            rear=(rear+1)%size;
             arr[rear]=data;
 
          }
@@ -33,33 +42,37 @@ public class Queue {
 
          public static int remove(){
 
-            if(rear==size-1){
+            if(isEmpty()){
                 System.out.println("Queue is empty");
                 return -1;
             }
 
-            int front=arr[0];
-            for(int i=0;i<rear;i++){
-                arr[i]=arr[i+1];
+            int result=arr[front];
+            
+
+            //last element delete
+            if(rear==front){
+                rear=front=-1;
+            }else{
+                front=(front+1)%size;
             }
-            rear=rear-1;
-            return front;
+            return result ;
          }
 
          //peek
          public static int peek(){
-            if(rear==size-1){
+            if(isEmpty()){
                 System.out.println("Queue is empty");
                 return -1;
             }
-            return arr[0];
+            return arr[front];
 
          }
 
     }
 
     public static void main(String [] args){
-        queue q=new queue(5);
+        queue q=new queue(3);
         q.add(1);
         q.add(2);
         q.add(3);
